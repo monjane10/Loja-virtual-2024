9c884e0279d931e56d37183980f69215c0769b20
monjane10 <louramonja52.n@gmail.com>
2024-11-13 23:56:32 +0200

    Envio de Email unsando Template(Email personalizado)

--- Backend/src/main/java/com/dev/Backend/service/EmailService.java
+++ Backend/src/main/java/com/dev/Backend/service/EmailService.java
@@ -1,10 +1,18 @@
 package com.dev.Backend.service;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import java.util.Map;
 
 @Service
 public class EmailService {
@@ -12,6 +20,8 @@ public class EmailService {
     private JavaMailSender javaMailSender;
     @Value("${spring.mail.username}")
     private String remetente;
+    @Autowired
+    private Configuration fmConfiguration;
 
     public String enviarEmailTexto(String destinatario, String titulo, String mensagem){
         try {
@@ -29,4 +39,34 @@ public class EmailService {
         }
 
     }
+
+    public void enviarEmailTemplate(String destinatario, String titulo, Map<String, Object> propriedades){
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setSubject(titulo);
+            mimeMessageHelper.setFrom(remetente);
+            mimeMessageHelper.setTo(destinatario);
+
+            mimeMessageHelper.setText(getContentFromTemplate(propriedades), true);
+            javaMailSender.send(mimeMessageHelper.getMimeMessage());
+        } catch (MessagingException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getContentFromTemplate(Map<String, Object> model) {
+        StringBuilder content = new StringBuilder();
+        try {
+            // Corrige a extensão para .ftl
+            Template template = fmConfiguration.getTemplate("email-recuperacao-codigo.flth");
+
+            // Passa o template e o model para processTemplateIntoString
+            content.append(FreeMarkerTemplateUtils.processTemplateIntoString(template, model));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
 }
